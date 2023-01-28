51fa9e64c7ba45d72e75fc32d960429b572260d4
Teddy Github <tkalpgithub@gmail.com>
2023-01-28 07:46:30 -0700

    fixes font issues with production build

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import GameProvider from "@/lib/game-context";
+import styles from "../styles/fonts.css";
+
 export default function App({ Component, pageProps }) {
   return (
     <GameProvider>
